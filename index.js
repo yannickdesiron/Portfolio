d069a5c12b32e0eb5e1b61b9d5930281143daa60
yannickdesiron <r0431949@student.thomasmore.be>
2025-03-30 18:02:18 +0200

    changes to index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import { createServer } from "http";
 import next from "next";
 
+const port = process.env.PORT || 3000;
 const app = next({ dev: false });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     handle(req, res);
-  }).listen(3000, () => {
-    console.log("Ready on http://localhost:3000");
+  }).listen(port, () => {
+    console.log(`🚀 Ready on http://localhost:${port}`);
   });
 });
